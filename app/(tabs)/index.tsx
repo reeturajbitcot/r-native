3979c07bef2a4dffe7457aef02364273d257cf9c
Reetu raj <reeturaj@bitcot.com>
2025-06-28 18:35:39 +0530

    third commit for the r-app

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,42 @@
-import { Text, View } from "react-native";
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import { useRouter } from "expo-router";
+import { ActivityIndicator, FlatListComponent, Image, ScrollView, Text, View } from "react-native";
 import "../globals.css";
  
 export default function App() {
+  const router = useRouter();
+  
+  const {data: movies, 
+        loading: moviesLoading,
+        error: moviesError
+        } = useFetch(() => fetchMovies({query: ""}))
+
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500 text-dark-200">
-        Welcome to Nativewind!
-      </Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0"/>
+      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight: "100%", paddingBottom: 10}}>
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+        {moviesLoading ? (
+          <ActivityIndicator size="large" color="#0000ff" className="mt-10 self-center" />
+        ): moviesError ? (
+          <Text>Error: {moviesError?.message}</Text>
+        ): (
+          <View className="flex-1 mt-5">
+            <SearchBar
+             onPress={() => router.push("/search")}
+             placeholder="Search for a movie"
+             />
+             <>
+              <Text className="text-lg text-whtie font-bold mt-5 mb-3">Latest Movies</Text>
+              <FlatListComponent></FlatListComponent>
+             </>
+          </View>
+        )}
+      </ScrollView>
     </View>
   );
 }
